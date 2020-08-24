39125fbf8a0cf9c0b59559e627d02b55ada1163a
Cristiano Amici <amicicristiano@gmail.com>
2020-08-24 12:50:27 -0500

    Detect whether to use curl or wget

--- install.sh
+++ install.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 
+BREWFILE_URL=https://raw.githubusercontent.com/AChris07/brew-config/master/Brewfile
+BREWFILE_NAME=$HOME/Brewfile
+
 if [ $(xcode-select -p &> /dev/null; printf $?) -ne 0 ]; then
   echo "XCode tools not found. Installing..."
   xcode-select --install
 fi
 
-if [[ $(command -v brew) == "" ]]; then 
+if [ $(command -v brew &> /dev/null; printf $?) -ne 0 ]; then
   echo "Installing Homebrew.. "
   bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
+
+if [ $(command -v curl &> /dev/null; printf $?) -ne 0 ]; then
+  HOMEBREW_SETUP=`curl $BREWFILE_URL -o $BREWFILE_NAME`
+elif [ $(command -v wget &> /dev/null; printf $?) -ne 0 ]; then
+  HOMEBREW_SETUP=`wget $BREWFILE_URL -O $BREWFILE_NAME`
+else
+  echo "Could not find curl or wget. Please install one of these and try again. Aborting..." 1>&2
+  exit 128
+fi
+
 echo "Setting up Homebrew..."
-curl https://raw.githubusercontent.com/AChris07/brew-config/master/Brewfile -o $HOME/Brewfile
+eval $HOMEBREW_SETUP
 brew bundle
 rm -fr $HOME/Brewfile
 echo "Homebrew is ready!"
